008834d322455e93e2d0d6338b65beac8d5005e1
GregoMur <GregoMur@yandex.ru>
2023-07-17 20:52:29 +0300

    fix: change url for mongoose.connect

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const { PORT=3000 } = process.env;
 // Create server
 const app = express();
 
+// Connect to mongo server
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb',  {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
 // listen port
 app.listen(PORT, () => {
   console.log(`Listening port - ${PORT}`);
