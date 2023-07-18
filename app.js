8363c37ce188901d6afde6efca5f49b41fc5124e
GregoMur <GregoMur@yandex.ru>
2023-07-18 21:36:12 +0300

    feat: add body parser middleware

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 // include routes "users" and "movies"
 const { users, movies } = require('./routes');
+// include body-parser
+const bodyParser = require('body-parser');
 
 // PORT
 const { PORT=3000 } = process.env;
 
 // Create server
 const app = express();
+
+//  body parser middleware
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.use(express.json());
 
 // user routes
 app.use('/users', users);
