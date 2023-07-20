907e6b698b0f923d0c05f6b16df8c86cabeee60a
GregoMur <GregoMur@yandex.ru>
2023-07-20 23:30:11 +0300

    fix: change comments

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,10 @@ const users = require('express').Router();
 //  include user's controllers
 const { getUserInfo, updateUserInfo } = require('../controllers/users');
 
-// get user info (email and name)
+// get - user info (email and name)
 users.get('/me', getUserInfo);
 
-// patch user info (email and name)
+// patch - user info (email and name)
 users.patch('/me', updateUserInfo);
 
 module.exports = users;
