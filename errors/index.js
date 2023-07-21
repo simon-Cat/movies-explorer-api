6a702deded5447638e023b3ae8e07831fce36c87
GregoMur <GregoMur@yandex.ru>
2023-07-21 21:44:55 +0300

    feat: add error folder

--- errors/index.js
+++ errors/index.js
@@ -1,7 +1,13 @@
 // Authorization error
 const AuthorizationError = require('./authorization-error');
+// Forbidden error
+const ForbiddenError = require('./forbidden-error');
+// Not found error
+const NotFoundError = require('./not-found-error');
 
 
 module.exports = {
   AuthorizationError,
+  ForbiddenError,
+  NotFoundError
 };
