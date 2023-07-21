6936f77a8d412c9c81029abb3175b1e459113c92
GregoMur <GregoMur@yandex.ru>
2023-07-22 00:16:01 +0300

    feat: add celebrate validation's methods

---
+++ utils/requestValidation.js
@@ -0,0 +1,54 @@
+// include celebrate, Joi
+const { celebrate, Segments, Joi } = require('celebrate');
+// include urlRegexp
+const urlRegexp = require('./urlRegexp');
+
+
+// signin
+module.exports.validateSignin = () => celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+});
+
+// signup
+module.exports.validateSignup = () => celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+// update user info
+module.exports.validateUpdateUserInfo = () => celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+// add new movie
+module.exports.validateAddMovie = () => celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(urlRegexp),
+    trailerLink: Joi.string().required().pattern(urlRegexp),
+    thumbnail: Joi.string().required().pattern(urlRegexp),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+// remove movie
+module.exports.validateRemoveMovie = () => celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    movieID:  Joi.string().hex().length(24).required(),
+  }),
+});
