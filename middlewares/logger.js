d95edc996336b33b435e99cd319053d4c8fecee8
GregoMur <GregoMur@yandex.ru>
2023-07-22 13:38:58 +0300

    feat: create request logger and error logger

---
+++ middlewares/logger.js
@@ -0,0 +1,25 @@
+// include winston
+const winston = require('winston');
+// include express-winston
+const expressWinston = require('express-winston');
+
+// request logger
+const requestLogger = expressWinston.logger({
+  transports: [
+    new winston.transports.File({ filename: 'request.log' }),
+  ],
+  format: winston.format.json(),
+});
+
+// error logger
+const errorLogger = expressWinston.logger({
+  transports: [
+    new winston.transports.File({ filename: 'error.log' }),
+  ],
+  format: winston.format.json(),
+});
+
+module.exports = {
+  requestLogger,
+  errorLogger,
+};
