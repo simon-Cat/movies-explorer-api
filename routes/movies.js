2db40cdb80d2fbd8f4bb85b9f2b0e1aaa9a41ab3
GregoMur <GregoMur@yandex.ru>
2023-07-22 13:23:33 +0300

    feat: add celebrate validation

--- routes/movies.js
+++ routes/movies.js
@@ -2,14 +2,16 @@
 const movies = require('express').Router();
 // include movie's controllers
 const { getMovies, addMovie, removeMovie } = require('../controllers/movies');
+// include "validateAddMovie", "validateRemoveMovie"
+const { validateAddMovie, validateRemoveMovie } = require('../utils/requestValidation');
 
 // get - all user's movies
 movies.get('/', getMovies);
 
 // post - add new movie
-movies.post('/', addMovie);
+movies.post('/', validateAddMovie(), addMovie);
 
 // delete - remove movie
-movies.delete('/:movieID', removeMovie)
+movies.delete('/:movieID', validateRemoveMovie(), removeMovie)
 
 module.exports = movies;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,14 @@
 const users = require('express').Router();
 //  include user's controllers
 const { getUserInfo, updateUserInfo } = require('../controllers/users');
+// include "validateUpdateUserInfo"
+const { validateUpdateUserInfo } = require('../utils/requestValidation');
+
 
 // get - user info (email and name)
 users.get('/me', getUserInfo);
 
 // patch - user info (email and name)
-users.patch('/me', updateUserInfo);
+users.patch('/me', validateUpdateUserInfo(), updateUserInfo);
 
 module.exports = users;
