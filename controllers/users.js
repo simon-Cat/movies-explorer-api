89ffadb230ac714c35c7eba5c137484c0b8704d2
GregoMur <GregoMur@yandex.ru>
2023-07-24 13:45:01 +0300

    fix: eslint check

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,5 @@
+// include NODE_ENV, JWT_SECRET
+const { NODE_ENV, JWT_SECRET } = process.env;
 // include Error Mongoose
 const { Error } = require('mongoose');
 // include bcryptjs
@@ -58,7 +60,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {
         expiresIn: '7d',
       });
       res.send({ token });
